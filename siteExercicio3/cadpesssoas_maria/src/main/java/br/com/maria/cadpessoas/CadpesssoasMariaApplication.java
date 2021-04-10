a93214c19b5a887db4c49a5fa136db7531c7a31c
mariali51 <80051322+mariali51@users.noreply.github.com>
2021-04-10 17:05:25 -0300

    .

---
+++ siteExercicio3/cadpesssoas_maria/src/main/java/br/com/maria/cadpessoas/CadpesssoasMariaApplication.java
@@ -0,0 +1,13 @@
+package br.com.maria.cadpessoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadpesssoasMariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadpesssoasMariaApplication.class, args);
+	}
+
+}
